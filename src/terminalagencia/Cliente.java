522cfd34f7487321eb7ed05a208b6b5b29884c22
adalovelace1843 <lo.gaste@gmail.com>
2017-09-08 11:49:04 -0300

    v2

--- src/terminalagencia/Cliente.java
+++ src/terminalagencia/Cliente.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,6 +26,55 @@ public class Cliente {
         String linea;
         teclado=new BufferedReader(new InputStreamReader(System.in));
         try {
+            for (int i =0; i<=6; i++){
+                switch(i){
+                    case 0: 
+                        System.out.println("Ingrese Matricula:");
+                        escritura=new PrintWriter(sock.getOutputStream(),true);
+                        linea=teclado.readLine();
+                        escritura.println(linea);
+                        break;
+                    case 1: 
+                        String ag_vta = "Pocitos";
+                        escritura=new PrintWriter(sock.getOutputStream(),true);
+                        linea=ag_vta;
+                        escritura.println(linea);
+                        break;
+                    case 2: 
+                        escritura=new PrintWriter(sock.getOutputStream(),true);
+                        Date fecha = new Date();
+                        escritura.println(fecha);
+                        break;
+                    case 3: 
+                        escritura=new PrintWriter(sock.getOutputStream(),true);
+                        Date fecha2 = new Date();
+                        escritura.println(fecha2);
+                        break;
+                    case 4: 
+                        System.out.println("Ingrese Minutos:");
+                        escritura=new PrintWriter(sock.getOutputStream(),true);
+                        linea=teclado.readLine();
+                        escritura.println(linea);
+                        break;
+                    case 5: 
+                        String term = "01";
+                        escritura=new PrintWriter(sock.getOutputStream(),true);
+                        linea=term;
+                        escritura.println(linea);
+                        break;
+                    case 6:
+                        escritura=new PrintWriter(sock.getOutputStream(),true);
+                        linea="quit";
+                        escritura.println(linea);
+                        break;
+                }
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        /*try {
+            
             System.out.println("Ingrese comando:");
             escritura=new PrintWriter(sock.getOutputStream(),true);
             linea=teclado.readLine();
@@ -32,7 +82,6 @@ public class Cliente {
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
             Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
+        }*/
     }
 }
